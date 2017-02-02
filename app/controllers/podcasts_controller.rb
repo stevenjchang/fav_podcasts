0192ac8df3948dd2f83a3ef820e2d901fdffcbf5
Steve <stevenjchang@gmail.com>
2017-02-02 20:46:39 +0800

    fixed controller, add .require.permit for new columns

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -71,6 +71,6 @@ class PodcastsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def podcast_params
       # params.fetch(:podcast, {})
-      params.require(:podcast).permit(:title, :host)
+      params.require(:podcast).permit(:title, :host, :description, :my_review, :url)
     end
 end
