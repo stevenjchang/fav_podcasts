887959c7462c312d0968782ea00107856d91db6a
Steve <stevenjchang@gmail.com>
2017-02-03 15:58:32 +0800

    Fixed: Clean_Blog template fully working under podcasts (views/controller), see Quiver notes for detailed solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
 
-  # resources :podcasts
+  resources :podcasts
 
   # root to: 'podcasts#index'
 
   # get 'stylish_portfolios/index'
 
-  root to: 'clean_blogs#index'
+  root to: 'podcasts#index'
   # get 'clean_blogs/index'
-  get 'about'    => 'clean_blogs#about'
-  get 'post'     => 'clean_blogs#post'
-  get 'contact'  => 'clean_blogs#contact'
+  get 'about'    => 'podcasts#about'
+  get 'post'     => 'podcasts#post'
+  get 'contact'  => 'podcasts#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
