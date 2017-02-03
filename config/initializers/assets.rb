d6983dc7246bbaeebc15414460c9669c95f457d0
Steve <stevenjchang@gmail.com>
2017-02-03 15:22:05 +0800

    fixed clean_blog issue => views/layouts/clean_blog.html.erb line9,10 added, manifest.js manifest.css added

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( clean_blog/manifest.js clean_blog/manifest.css )
 Rails.application.config.assets.precompile += %w( stylish_portfolio/manifest.css stylish_portfolio/manifest.js )
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
 
   # root to: 'podcasts#index'
 
-  get 'stylish_portfolios/index'
+  # get 'stylish_portfolios/index'
+
+  root to: 'clean_blogs#index'
+  # get 'clean_blogs/index'
+  get 'about'    => 'clean_blogs#about'
+  get 'post'     => 'clean_blogs#post'
+  get 'contact'  => 'clean_blogs#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
