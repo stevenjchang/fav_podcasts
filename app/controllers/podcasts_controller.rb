8a737c332626dac0fc919360de70b3841bfaecfd
Steve <stevenjchang@gmail.com>
2017-02-05 20:40:53 +0800

    Edit best_episodes to working template rendering

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -62,6 +62,10 @@ class PodcastsController < ApplicationController
     end
   end
 
+  def best_episodes
+    @podcasts = Podcast.all
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_podcast
